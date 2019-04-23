0e4647203e5f53cc9484a24bf6e47a32a4258bc5
Melanie Sumner <melaniersumner@gmail.com>
2019-04-23 13:14:23 -0500

    adding styles

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,29 +1,13 @@
 import DS from 'ember-data';
-// import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
-
-// import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 
 export default DS.JSONAPIAdapter.extend({
-  // fastboot: service(),
-
-  host: computed('fastboot.isFastBoot', function() {
-    if (this.get('fastboot.isFastBoot')) {
-      let protocol = this.get('fastboot.request.protocol');
-
-      return `${protocol}//${this.get('fastboot.request.host')}`;
-    } else {
-      return window.location.origin;
-    }
-  }),
-
   urlForFindAll(modelName) {
     const path = this.pathForType(modelName);
-    return `${this.host}/${path}/all.json`;
+    return `${path}/all.json`;
   },
 
   urlForFindRecord(id, modelName) {
     const path = this.pathForType(modelName);
-    return `${this.host}/${path}/${id}.json`;
+    return `${path}/${id}.json`;
   }
 });
